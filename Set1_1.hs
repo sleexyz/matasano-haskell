fe17dbcea32c7dae39b7a425de0f8f1fbf0a55f0
Sean Lee <freshdried@gmail.com>
2016-02-15 23:57:30 -0500

    added test case

--- Set1_1.hs
+++ Set1_1.hs
@@ -1,6 +1,10 @@
 import Numeric
 
 
+hexInput = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+expectedB64Output = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+
+
 -- | myReadHex is just what readHex should be intuitively
 myReadHex :: String -> Integer
 myReadHex = fst . head . readHex
